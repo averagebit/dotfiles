36bd345bc2396d723a6977329bfb65ea184d5316
averagebit <averagebit@pm.me>
2023-04-18 19:49:28 +0000

    fix(nvim): update home-network repo path

--- home/private_dot_config/nvim/lua/user/alpha.lua
+++ home/private_dot_config/nvim/lua/user/alpha.lua
@@ -17,7 +17,7 @@ dashboard.section.buttons.val = {
 	dashboard.button("e", " " .. " New file", "<cmd>ene <BAR> startinsert<CR>"),
 	dashboard.button("r", " " .. " Recent files", "<cmd>Telescope oldfiles<CR>"),
 	dashboard.button("h", "ﴤ " .. " Home", "<cmd>cd ~/.dot | :e home/dot_bashrc<CR>"),
-	dashboard.button("n", "歷" .. " Network", "<cmd>cd ~/Workspace/home/network | :e README.md<CR>"),
+	dashboard.button("n", "歷" .. " Network", "<cmd>cd ~/Workspace/home/home-network | :e README.md<CR>"),
 	dashboard.button("l", "" .. "  Laboratory", "<cmd>cd ~/Workspace/home/home-ops | :e README.md<CR>"),
 	dashboard.button("c", " " .. " Configuration", "<cmd>cd ~/.dot/home/private_dot_config/nvim | :e init.lua<CR>"),
 	dashboard.button("q", " " .. " Quit", "<cmd>qa<CR>"),
