3517c23aaa0317bae46566f8f946efda05135c4c
averagebit <averagebit@pm.me>
2023-04-19 05:44:29 +0000

    fix(chezmoi): fix setup script

--- setup.sh
+++ setup.sh
@@ -5,8 +5,9 @@ set -euo pipefail
 if ! type nix &>/dev/null; then
     curl -L https://nixos.org/nix/install | sh -s -- --no-modify-profile --no-daemon  --yes
     source "${HOME}/.nix-profile/etc/profile.d/nix.sh"
-    nix-channel --add https://nixos.org/channels/nixpkgs-unstable
 fi
 
+git clone git@github.com:averagebit/dotfiles "${HOME}/.dot"
+cd "${HOME}/.dot"
 nix build --experimental-features "nix-command flakes"
 exec ./result/bin/chezmoi init --ssh --source ~/.dot --force --apply averagebit
