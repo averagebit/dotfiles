f5c15f655460a9d176a0d81d0f9f17b9c1c584f5
averagebit <averagebit@pm.me>
2023-04-20 09:14:24 +0000

    chore(lint): format flake.nix

--- flake.nix
+++ flake.nix
@@ -1,16 +1,11 @@
 {
   description = "Dotfiles package dependencies";
 
-  inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
-  };
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable"; };
 
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
-    in
-    {
-      packages.${system}.default = pkgs.chezmoi;
-    };
+    in { packages.${system}.default = pkgs.chezmoi; };
 }
