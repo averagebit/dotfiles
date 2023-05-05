51a3fbafe642866b6f601d0bb9fbe7370f9faa47
averagebit <averagebit@pm.me>
2023-05-05 23:11:48 +0000

    feat(nvim): add default lsp diagnostic icons

--- home/private_dot_config/nvim/lua/user/lsp.lua
+++ home/private_dot_config/nvim/lua/user/lsp.lua
@@ -16,6 +16,52 @@ function M.config()
     capabilities.textDocument.completion.completionItem.snippetSupport = true
     capabilities = cmp_nvim_lsp.default_capabilities(M.capabilities)
 
+    local signs = {
+        { name = "DiagnosticSignError", text = "" },
+        { name = "DiagnosticSignWarn", text = "" },
+        { name = "DiagnosticSignHint", text = "" },
+        { name = "DiagnosticSignInfo", text = "" },
+    }
+
+    for _, sign in ipairs(signs) do
+        vim.fn.sign_define(
+            sign.name,
+            { texthl = sign.name, text = sign.text, numhl = "" }
+        )
+    end
+
+    local cfg = {
+        virtual_text = true,
+        signs = {
+            active = signs,
+        },
+        update_in_insert = true,
+        underline = true,
+        severity_sort = true,
+        float = {
+            focusable = true,
+            style = "minimal",
+            border = "rounded",
+            source = "always",
+            header = "",
+            prefix = "",
+        },
+    }
+
+    vim.diagnostic.config(cfg)
+
+    vim.lsp.handlers["textDocument/hover"] =
+        vim.lsp.with(vim.lsp.handlers.hover, {
+            border = "rounded",
+            width = 60,
+        })
+
+    vim.lsp.handlers["textDocument/signatureHelp"] =
+        vim.lsp.with(vim.lsp.handlers.signature_help, {
+            border = "rounded",
+            width = 60,
+        })
+
     local function lsp_keymaps(bufnr)
         local opts = { noremap = true, silent = true }
         local keymap = vim.api.nvim_buf_set_keymap
