cc376cb151364d0bdc8f1233411a39505e87bfe0
averagebit <averagebit@pm.me>
2023-07-09 15:54:37 +0000

    feat!(dotfiles): the museum lives

--- home/private_dot_config/nvim/lua/autocommands.lua
+++ home/private_dot_config/nvim/lua/autocommands.lua
@@ -3,9 +3,9 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
     pattern = { "qf", "help", "man", "lspinfo", "spectre_panel" },
     callback = function()
         vim.cmd([[
-      nnoremap <silent> <buffer> q :close<CR>
-      set nobuflisted
-    ]])
+            nnoremap <silent> <buffer> q :close<CR>
+            set nobuflisted
+        ]])
     end,
 })
 
@@ -17,6 +17,7 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
         vim.opt_local.spell = true
     end,
 })
+
 -- Automatically close tab/vim when nvim-tree is the last window in the tab
 vim.cmd(
     "autocmd BufEnter * ++nested if winnr('$') == 1 && bufname() == 'NvimTree_' . tabpagenr() | quit | endif"
