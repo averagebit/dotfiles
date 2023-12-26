889746f42e34f173dccecc2e04d6e2db47708548
averagebit <averagebit@pm.me>
2023-12-26 09:57:42 +0000

    feat(sway): sway returns

---
+++ home/private_dot_config/nixpkgs/config.nix
@@ -0,0 +1,107 @@
+{
+  allowUnfree = true;
+  packageOverrides = pkgs:
+    with pkgs; {
+      user = pkgs.buildEnv {
+        name = "user-packages";
+        paths = [
+          age
+          awscli2
+          bind
+          bitwarden-cli
+          cargo
+          chezmoi
+          cinnamon.nemo
+          clippy
+          curl
+          delve
+          direnv
+          exiftool
+          fd
+          fluxcd
+          foot
+          fuzzel
+          fzf
+          gammastep
+          gcc
+          gh
+          git
+          git-lfs
+          gnumake
+          go
+          goaccess
+          gzip
+          hadolint
+          helm
+          htop
+          inxi
+          jq
+          k9s
+          kind
+          kubeconform
+          kubectl
+          kustomize
+          lazygit
+          libredwg
+          lm_sensors
+          lsof
+          lynx
+          mako
+          mpv
+          neofetch
+          neovim
+          netlify-cli
+          nettools
+          nix-direnv
+          nixfmt
+          nmap
+          nodejs
+          p7zip
+          packer
+          pandoc
+          pavucontrol
+          pciutils
+          perl
+          podman
+          podman-compose
+          pulumi-bin
+          ripgrep
+          rsync
+          rust-analyzer
+          rustc
+          rustfmt
+          shellcheck
+          smartmontools
+          sops
+          sshpass
+          stack
+          strace
+          stylua
+          swayimg
+          sysfsutils
+          terraform
+          tmux
+          traceroute
+          tree
+          tree-sitter
+          tz
+          unrar
+          unzip
+          ventoy
+          vhs
+          vial
+          vscode
+          weechat
+          wget
+          whois
+          wl-clipboard
+          yq
+          yt-dlp
+          ytfzf
+          zathura
+          zip
+        ];
+        # pathsToLink = [ "/bin" "/share" ];
+      };
+    };
+}
