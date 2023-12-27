9bc970ada67e94fa5466a1255d5e4e9ffe049521
averagebit <averagebit@pm.me>
2023-12-27 12:45:56 +0000

    feat(nvim): set commit filetype line length

--- home/private_dot_config/nvim/lua/user/autocmds.lua
+++ home/private_dot_config/nvim/lua/user/autocmds.lua
@@ -13,6 +13,7 @@ vim.api.nvim_create_autocmd({ "FileType" }, {
 vim.api.nvim_create_autocmd({ "FileType" }, {
     pattern = { "gitcommit", "markdown" },
     callback = function()
+        vim.opt_local.textwidth = 72
         vim.opt_local.wrap = true
         vim.opt_local.spell = true
     end,
