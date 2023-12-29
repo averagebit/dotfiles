3d0b2982d6432eec0c3b2a0c7b1085e426995864
averagebit <averagebit@pm.me>
2023-12-29 18:36:17 +0000

    feat(nvim): ignore specific languages in treesitter

--- home/private_dot_config/nvim/lua/user/treesitter.lua
+++ home/private_dot_config/nvim/lua/user/treesitter.lua
@@ -43,25 +43,23 @@ function M.config()
         -- parser_install_dir = "/some/path/to/store/parsers", -- Remember to run vim.opt.runtimepath:append("/some/path/to/store/parsers")!
 
         highlight = {
-            -- `false` will disable the whole extension
             enable = true,
-
-            -- NOTE: these are the names of the parsers and not the filetype. (for example if you want to
-            -- disable highlighting for the `tex` filetype, you need to include `latex` in this list as this is
-            -- the name of the parser)
-            -- list of language that will be disabled
-            -- disable = { "css" },
-
-            -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
+            -- Disable for files larger than `max_filesize` or if it matches one
+            -- of the languages in `langs`.
             disable = function(lang, buf)
+                local langs = { "markdown" }
                 local max_filesize = 100 * 1024 -- 100 KB
+                for _, value in ipairs(langs) do
+                    if lang == value[1] then
+                        return true
+                    end
+                end
                 local ok, stats =
                     pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
                 if ok and stats and stats.size > max_filesize then
                     return true
                 end
             end,
-
             -- Setting this to true will run `:h syntax` and tree-sitter at the same time.
             -- Set this to `true` if you depend on 'syntax' being enabled (like for indentation).
             -- Using this option may slow down your editor, and you may see some duplicate highlights.
